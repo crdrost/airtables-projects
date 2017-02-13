99cd8dc4af156934a8077555a43a139d6ee4879c
CR Drost <chris.drostie@gmail.com>
2017-02-12 21:01:48 -0600

    Preliminary successes?

---
+++ app/Main.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main  where
+
+import Airtable.Query
+import Data.Aeson
+
+creds :: AirtableOptions
+creds = AirtableOptions "keypGWoHSlUbWVS6Z" "apprFH3kxVr5DVE2U" 0
+
+data Thread = Thread {tName :: String, tContainedIn :: [String], tContainments :: [String], tDescription :: String}
+
+instance FromJSON Thread where
+    parseJSON (Object o) = Thread <$> o .: "Thread name"
+                                  <*> o .: "Contained In"
+                                  <*> o .: "Containments (raw)"
+                                  <*> o .: "Description"
+
+
+someFunc = putStrLn "Hello, Stack!"
+
+
+main :: IO ()
+main = someFunc
+
