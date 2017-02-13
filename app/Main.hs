6a02a7041fd109390cb79665e2d6c6ded9b132b6
CR Drost <chris.drostie@gmail.com>
2017-02-12 23:26:57 -0600

    Working Aeson parsing.

--- app/Main.hs
+++ app/Main.hs
@@ -8,13 +8,25 @@ import Data.Aeson
 creds :: AirtableOptions
 creds = AirtableOptions "keypGWoHSlUbWVS6Z" "apprFH3kxVr5DVE2U" 0
 
-data Thread = Thread {tName :: String, tContainedIn :: [String], tContainments :: [String], tDescription :: String}
+data Thread = Thread {
+    tName :: String,
+    tContainedIn :: [String],
+    tContainments :: [String],
+    tDescription :: String,
+    tArchived :: Bool,
+    tStatus :: String
+} deriving (Show)
+
+
+getWithDefault obj key def = maybe def id <$> obj .:? key 
 
 instance FromJSON Thread where
-    parseJSON (Object o) = Thread <$> o .: "Thread name"
-                                  <*> o .: "Contained In"
-                                  <*> o .: "Containments (raw)"
-                                  <*> o .: "Description"
+    parseJSON (Object f) = Thread <$> getWithDefault f "Thread name" ""
+                                  <*> f .: "Contained in"
+                                  <*> f .: "Containments (raw)"
+                                  <*> getWithDefault f "Description" ""
+                                  <*> getWithDefault f "Archived?" False
+                                  <*> getWithDefault f "Status" ""
 
 
 someFunc = putStrLn "Hello, Stack!"
